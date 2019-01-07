f04eeff4b2b92a137fa52983e72e7cd3c7bc50b4
Rushik Maniar <rushikmaniar107@gmail.com>
2019-01-07 13:12:43 +0530

    little changes in PendingRequest

--- application/controllers/mobile/PendingRequest.php
+++ application/controllers/mobile/PendingRequest.php
@@ -74,4 +74,133 @@ class PendingRequest extends MobileController
         echo json_encode($this->response_array);
         exit;
     }
+
+    /*
+     * get list of pending request
+     * post data :
+     *  family_tree_id
+     * */
+    public function getRequestList()
+    {
+        if( (isset($_POST['family_tree_id']))){
+            $userdata = $this->session->userdata('vanshavali-mobile');
+
+            //check if user is author of familt_tree
+            $family_where = array(
+                'family_tree_ownerid'=>$userdata['user_id'],
+                'family_tree_id'=>$_POST['family_tree_id']
+            );
+            $check_owner = $this->CommonModel->getRecord('family_trees',$family_where)->num_rows();
+            if($check_owner == 1){
+                //user is owner of family . show request list
+                $where = array(
+                    'request_for_family_id'=>$_POST['family_tree_id']
+                );
+                $pending_request = $this->CommonModel->getRecord('pending_request',$where)->result_array();
+                $this->response_array['vanshavali_response']['data'] = $pending_request;
+                $this->response_array['vanshavali_response']['code'] = 200;
+                $this->response_array['vanshavali_response']['message'] = 'Status 200 Ok. Pending_request Feteched';
+
+            }else{
+                //user is not owner of family
+                //error 403 Forbidden.
+                $this->response_array['vanshavali_response']['code'] = 403;
+                $this->response_array['vanshavali_response']['message'] = 'Error 403 Forbidden. User Not Authorised To view Family';
+            }
+
+        }else{
+            //error 400 bad reqest . invalid parameters
+            $this->response_array['vanshavali_response']['code'] = 400;
+            $this->response_array['vanshavali_response']['message'] = 'Error 400 BadRequest. Invalid Parameters Passed';
+        }
+        echo json_encode($this->response_array);
+        exit;
+    }
+
+    /*
+     * delete pending request
+     * post data :
+     *  family_tree_id
+     *  pending_request_id
+     * */
+    public function deleteRequest()
+    {
+        if( (isset($_POST['family_tree_id'])) && (isset($_POST['request_id']))){
+            $userdata = $this->session->userdata('vanshavali-mobile');
+
+            //check if user is author of familt_tree
+            $family_where = array(
+                'family_tree_ownerid'=>$userdata['user_id'],
+                'family_tree_id'=>$_POST['family_tree_id']
+            );
+            $check_owner = $this->CommonModel->getRecord('family_trees',$family_where)->num_rows();
+            if($check_owner == 1){
+                //user is owner of family . allow to delte request
+                $where = array(
+                    'id'=>$_POST['request_id']
+                );
+                $pending_request = $this->CommonModel->delete('pending_request',$where);
+                $this->response_array['vanshavali_response']['code'] = 200;
+                $this->response_array['vanshavali_response']['message'] = 'Status 200 Ok. Pending request Deleted';
+
+            }else{
+                //user is not owner of family
+                //error 403 Forbidden.
+                $this->response_array['vanshavali_response']['code'] = 403;
+                $this->response_array['vanshavali_response']['message'] = 'Error 403 Forbidden. User Not Authorised To Delete Pending Request';
+            }
+
+        }else{
+            //error 400 bad reqest . invalid parameters
+            $this->response_array['vanshavali_response']['code'] = 400;
+            $this->response_array['vanshavali_response']['message'] = 'Error 400 BadRequest. Invalid Parameters Passed';
+        }
+        echo json_encode($this->response_array);
+        exit;
+    }
+
+    /*
+    * make  pending request as viewd by owner of family only
+    * post data :
+    *  family_tree_id
+    *  pending_request_id
+    * */
+    public function makeRequestViewed()
+    {
+        if( (isset($_POST['family_tree_id'])) && (isset($_POST['request_id']))){
+            $userdata = $this->session->userdata('vanshavali-mobile');
+
+            //check if user is author of familt_tree
+            $family_where = array(
+                'family_tree_ownerid'=>$userdata['user_id'],
+                'family_tree_id'=>$_POST['family_tree_id']
+            );
+            $check_owner = $this->CommonModel->getRecord('family_trees',$family_where)->num_rows();
+            if($check_owner == 1){
+                //user is owner of family . allow to delte request
+                $where = array(
+                    'id'=>$_POST['request_id']
+                );
+                $pending_request = $this->CommonModel->update('pending_request',array('is_viewed'=>1),$where);
+                $this->response_array['vanshavali_response']['code'] = 200;
+                $this->response_array['vanshavali_response']['message'] = 'Status 200 Ok. Pending request Deleted';
+
+            }else{
+                //user is not owner of family
+                //error 403 Forbidden.
+                $this->response_array['vanshavali_response']['code'] = 403;
+                $this->response_array['vanshavali_response']['message'] = 'Error 403 Forbidden. User Not Authorised To Delete Pending Request';
+            }
+
+        }else{
+            //error 400 bad reqest . invalid parameters
+            $this->response_array['vanshavali_response']['code'] = 400;
+            $this->response_array['vanshavali_response']['message'] = 'Error 400 BadRequest. Invalid Parameters Passed';
+        }
+        echo json_encode($this->response_array);
+        exit;
+    }
+
+
+
 }
