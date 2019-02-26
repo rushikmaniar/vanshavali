dff7855d8499f9e0112d29229841088bae0e25c9
rushik maniar <rushikmaniar107@gmail.com>
2019-02-26 22:21:52 +0530

    insert member in progress

--- application/controllers/mobile/MembersManage.php
+++ application/controllers/mobile/MembersManage.php
@@ -63,4 +63,51 @@ class MembersManage extends MobileController
         exit;
     }
 
+    public function addFamilyMember()
+    {
+        if( (isset($_POST['family_id']))  && (isset($_POST['member_name'])) && (isset($_POST['member_parent_id'])) && ($_POST['member_gender'])  ){
+            //check if user logged in can_view family
+            $family_access_where = array(
+                'user_id'=>$this->userdata['user_id'],
+                'family_id'=>$_POST['family_id']
+            );
+            $family_access = $this->CommonModel->getRecord('family_access_table',$family_access_where)->row_array();
+
+            //check id can_view = 1
+            if($family_access['can_insert'] == 1){
+                //user Authorised to view Family
+                $member_list_where = array(
+                    'member_family_tree_id'=>$_POST['family_id']
+                );
+                $member_name = $_POST['member_name'];
+                $member_gender = $_POST['member_gender'];
+                $member_parent_id = $_POST['member_parent_id'];
+                $member_family_id = $_POST['family_id'];
+
+
+                $member_data = array(
+                    'member_parent_id'=>$member_parent_id,
+                    'member_family_tree_id'=>$member_family_id,
+                    'member_gender'=>$member_gender,
+                    'member_full_name'=>$member_name
+                );
+                $member_insert = $this->CommonModel->save('member_list',$member_data);
+
+                $this->response_array['vanshavali_response']['code'] = 200;
+                $this->response_array['vanshavali_response']['message'] = 'Status 200 Ok. Members Added Succcessfuly';
+            }else{
+                //else user is not Authorised to view Family . Error 403 Forbidden
+                $this->response_array['vanshavali_response']['code'] = 403;
+                $this->response_array['vanshavali_response']['message'] = 'Error 403 Forbidden . User Not Authorised';
+            }
+        }else{
+            //Invalid Paramenter Error 400 Bad Request.
+            $this->response_array['vanshavali_response']['code'] = 400;
+            $this->response_array['vanshavali_response']['message'] = 'Error 400 Bad request. Invalid Parameters';
+
+        }
+        echo json_encode($this->response_array);
+        exit;
+    }
+
 }
